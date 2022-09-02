0b09b933892f45e3d00b9e522f10fe66d69bf921
onur <kizilayonurtahsin@gmaik.com>
2022-09-03 01:07:36 +0300

    v1

---
+++ app.js
@@ -0,0 +1,41 @@
+// document.getElementById("click").onclick = function () {
+//   fun();
+// };
+// function fun() {
+//   document
+//     .getElementById("click")
+//     .$("#body")
+//     .css("background-image", "url('./img/tumblr_piwv92QrU91qeyvpto1_500.gif')");
+//   document.getElementById("click").style.backgroundColor = "yellow";
+//   document.getElementById("click").style.fontSize = "25px";
+//   document.getElementById("click").style.border = "4px solid red";
+// }
+
+function myFunction() {
+  document.getElementById("bodyy").style.backgroundImage =
+    "url(./img/157265.jpg)";
+  //   document.getElementById("click").style.backgroundColor = "yellow";
+  //   document.getElementById("click").style.fontSize = "25px";
+  //   document.getElementById("click").style.border = "4px solid red";
+}
+
+function myFunction2() {
+  document.getElementById("bodyy").style.backgroundImage =
+    "url(./img/159081.jpg)";
+  //   document.getElementById("click1").style.backgroundColor = "purple";
+}
+function myFunction3() {
+  document.getElementById("bodyy").style.backgroundImage =
+    "url(./img/2068569.jpg)";
+  //   document.getElementById("click2").style.backgroundColor = "azure";
+}
+function myFunction4() {
+  document.getElementById("bodyy").style.backgroundImage =
+    "url(./img/ultra-hd-cyberpunk-futuristic-city-5k-wallpaper-11624392501x6ivl2gw9o.jpg)";
+  //   document.getElementById("click2").style.backgroundColor = "azure";
+}
+function myFunction5() {
+  document.getElementById("bodyy").style.backgroundImage =
+    "url(./img/wallpapersden.com_cyberpunk-2077-hd-gaming-22_2560x1440.jpg)";
+  //   document.getElementById("click2").style.backgroundColor = "azure";
+}
